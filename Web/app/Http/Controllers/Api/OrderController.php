e1c34834d8d81ca122c0fee7e534bbc2190fdf44
Bintangputraa <bintang.cahya.putraa@gmail.com>
2026-09-09 00:13:18 +0700

    Update OrderController discount

--- Web/app/Http/Controllers/Api/OrderController.php
+++ Web/app/Http/Controllers/Api/OrderController.php
@@ -77,6 +77,8 @@ class OrderController extends Controller
                 
                 $promo->increment('used');
             }
+        } else {
+            $discountValue = 0;
         }
 
         $order = DB::transaction(function () use ($data, $discountValue, $request) {
