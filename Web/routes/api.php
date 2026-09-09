ed2bf180ea0bea1782e4adf9f3ff8cbfb50e2c3d
Bintangputraa <bintang.cahya.putraa@gmail.com>
2026-09-10 03:14:22 +0700

    update route favorite

--- Web/routes/api.php
+++ Web/routes/api.php
@@ -27,8 +27,8 @@ Route::middleware('api.token')->group(function (): void {
     Route::post('auth/logout', [AuthController::class, 'logout'])->name('api.auth.logout');
 
     Route::apiResource('carts', CartController::class)->only(['index', 'store', 'update', 'destroy']);
-    Route::get('orders/favorites', [CustomerOrderController::class, 'favorites'])->name('orders.favorites');
-    Route::post('favorites/{itemId}/toggle', [CustomerOrderController::class, 'toggleFavorite'])->name('favorites.toggle');
+    Route::get('orders/favorites', [CustomerOrderController::class, 'favorites'])->name('orders.favoritesApp');
+    Route::post('favorites/{itemId}/toggle', [CustomerOrderController::class, 'toggleFavorite'])->name('favorites.toggleApp');
     Route::post('orders', [OrderController::class, 'store'])->name('api.orders.store');
     Route::get('orders/history', [CustomerOrderController::class, 'historyApp'])->name('orders.historyApp');
     Route::get('notifications', [NotificationController::class, 'index'])->name('api.notifications.index');
